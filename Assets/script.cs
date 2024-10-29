e266effdbb47614065532f373bff710b9c00d45a
Sondos Aljaru <sondosaljaru@gmail.com>
2024-10-29 12:50:10 +0200

    first commit

---
+++ Assets/script.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int playerLives = 3;
+
+        while (playerLives > 0)
+        {
+            if (playerLives == 3)
+            {
+                Debug.Log("Many lives");
+
+            }
+
+            if (playerLives == 1)
+            {
+                Debug.Log("The last life!");
+
+            }
+            playerLives--;
+
+
+
+        }
+        Debug.Log("Game Over!");
+
+    }
+
+
+}
